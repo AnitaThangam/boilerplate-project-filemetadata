c93814dfb2dec1918079a77725407f308b72d83e
AnitaThangam <anitachandrant@gmail.com>
2026-06-22 12:13:29 +0000

    Complete File Metadata Microservice

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const multer = require('multer');
 
-var app = express();
+require('dotenv').config();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
